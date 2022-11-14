6f5488fae16662104be275a1a6218e52e6fcd7ee
HankWeii <best0980276@gmail.com>
2022-11-15 02:26:52 +0800

    second commit

--- firuta-.js
+++ firuta-.js
@@ -1,89 +1,55 @@
 let boxstyle = document.querySelectorAll(".box");
 
+let chickennuggetsSwitch = {switch:false};
+let friesSwitch = {switch:false};
+let friedchickenSwitch = {switch:false};
+let hamburgerSwitch = {switch:false};
 
-let ChickenNuggets = document.querySelector(".chickennuggets");
+let chickennuggets = document.querySelector(".chickennuggets");
 let fries = document.querySelector(".fries");
 let friedchicken = document.querySelector(".friedchicken");
-let drink = document.querySelector(".drink");
+let hamburger = document.querySelector(".hamburger");
 
-let chickennuggetsSwitch = false;
-let friesSwitch = false;
-let friedchickenSwitch = false;
-let drinkSwitch = false;
+chickennuggets.addEventListener("click", function() {
+    colorChange(chickennuggetsSwitch, chickennuggets, '雞塊');
+});
 
-ChickenNuggets.onclick = chickennuggetsSearchOn;
-fries.onclick = friesSearchOn;
-friedchicken.onclick = friedchickenSearchOn;
-drink.onclick = drinkSearchOn;
+fries.addEventListener("click", function() {
+    colorChange(friesSwitch, fries, '薯');
+});
 
-function SearchKeyWords(keywords) {
-    for(let i=0; i<boxstyle.length; i++){
+friedchicken.addEventListener("click", function() {
+    colorChange(friedchickenSwitch, friedchicken, '炸雞');
+});
+
+hamburger.addEventListener("click", function() {
+    colorChange(hamburgerSwitch, hamburger, '堡');
+});
+
+function colorChange(foodSwitch, food, keywords) {
+    if (!foodSwitch.switch) {
+        food.style.backgroundColor = "#b4b4b4";
+        foodSwitch.switch = true;
+        searchKeyWords(keywords);
+    } else {
+        food.style.backgroundColor = "";
+        foodSwitch.switch = false;
+        hide();
+    }
+}
+
+function searchKeyWords(keywords) {
+    for (let i = 0; i < boxstyle.length; i++) {
         let discountText = boxstyle[i].innerHTML;
         let Searchkeyword = discountText.search(keywords);
-        if(Searchkeyword < 0){
+        if (Searchkeyword < 0) {
             boxstyle[i].style.display = "none";
         }
     }
 }
 
-function hide(){
-    for(let i=0; i<boxstyle.length; i++){
+function hide() {
+    for (let i = 0; i < boxstyle.length; i++) {
         boxstyle[i].style.display = "block";
-    }
-}
-
-function ColorChangeOn(food){
-    food.style.backgroundColor = "#b4b4b4";
-}
-
-function ColorChangeOff(food){
-    food.style.backgroundColor = "";
-}
-
-function chickennuggetsSearchOn(){
-    if(!chickennuggetsSwitch){
-        SearchKeyWords("雞塊");
-        ColorChangeOn(ChickenNuggets);
-        chickennuggetsSwitch = true;
-    } else {
-        ColorChangeOff(ChickenNuggets);
-        chickennuggetsSwitch = false;
-        hide();
-    }
-}
-
-function friesSearchOn(){
-    if(!friesSwitch){
-        fries.style.backgroundColor = "#b4b4b4";
-        friesSwitch = true;
-        SearchKeyWords("薯");
-    }else {
-        fries.style.backgroundColor = "";
-        friesSwitch = false;
-        hide();
-    }
-}
-
-function friedchickenSearchOn(){
-    if(!friedchickenSwitch){
-        friedchicken.style.backgroundColor = "#b4b4b4";
-        friedchickenSwitch = true;
-        SearchKeyWords("炸雞");
-    }else {
-        friedchicken.style.backgroundColor = "";
-        friedchickenSwitch = false;
-        hide();
-    }
-}
-
-function drinkSearchOn(){
-    if(!drinkSwitch){
-        drink.style.backgroundColor = "#b4b4b4";
-        drinkSwitch = true;
-        SearchKeyWords("飲");
-    }else {
-        drink.style.backgroundColor = "";
-        drinkSwitch = false;
-        hide();
     }
 }
